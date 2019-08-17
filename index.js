44198ec5d5e3b944129b7276fde7b8bded2e050d
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-16 20:14:00 -0700

    secondary commit

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 });
 
+
+const PORT  = process.env.PORT || 5000;
 app.listen(5000);
 
 //http://localhost:5000
+
+//req is the request
+
+//res is the data
